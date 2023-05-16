b002fd1832df649f638d702f6c1740626148de53
Ion Pascari <ionpascari1995@gmail.com>
2023-05-16 21:38:31 +0300

    feat: set up the spring cloud config server

---
+++ src/test/java/inc/evil/cnsconfigservice/CnsConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.evil.cnsconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnsConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
